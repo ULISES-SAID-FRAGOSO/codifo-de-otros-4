4ec53e4826a44712a90e1fb2ebe28669f91d8e0d
ulises <ulisessaix@gmail.com>
2024-09-27 17:15:42 -0600

    se modifica codigo otros 4

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,39 +1,50 @@
 package com.generation;
 
+import java.util.Scanner; //se importa scanner
+
 public class Codigo4 {
-
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
-
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
-    }
+	
+	public static void main(String[] args) { //se agrega metodo main
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+		String j1 = s.nextLine();
+		
+		System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");//se cambia a jugador 2
+		//Scanner s2 = new Scanner(); //se quita este sacnner
+		String j2 = s.nextLine();
+		
+		if (j1 == j2) {//se quita parentesis demas
+			System.out.println("Empate");
+		} else {
+			int g = 2;
+			switch(j1) {
+			case "piedra":
+				if (j2 == "tijeras") {
+					g = 1;
+				}
+				break; //se añade break
+				
+			case "papel":
+				if (j2 == "piedra") {
+					g = 1;
+				}
+					break;//se añade break
+					
+				case "tijera":
+					if (j2.equals("papel")) {
+						g = 1;
+					}
+					break;
+				default:
+					s.close();
+					return;
+				}
+				System.out.println("Gana el jugador " + g);
+			}
+		s.close();
+	}
   
   
 }
